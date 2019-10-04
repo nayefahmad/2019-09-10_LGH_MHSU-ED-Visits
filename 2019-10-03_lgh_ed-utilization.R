aa52439f04f25155934392b3dd5143bf7facab88
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-10-03 18:38:08 -0700

    join grouped start and end hours back on the skeleton

--- 2019-10-03_lgh_ed-utilization.R
+++ 2019-10-03_lgh_ed-utilization.R
@@ -37,25 +37,118 @@ setup_denodo()
 #' 
 #' # Skeleton data frame 
 #' 
+#' ## Parameters
+#' 
 
-start_date <- ymd_hms("2018-01-01 00:00:00")
-end_date <- ymd_hms("2018-01-03 00:00:00")  
+# Skeleton data frame -------
+start_date_time_p <- ymd_hms("2018-01-01 00:00:00")
+end_date_time_p <- ymd_hms("2018-01-02 00:00:00")  
 
+start_date_p <- ymd(start_date_time_p)
+end_date_p <- ymd(end_date_time_p)
+
+start_date_id_p <- gsub("-", "", start_date_p)
+end_date_id_p <- gsub("-", "", end_date_p)
+
+#' 
+#' ## Sequence of timestamps 
 df1.skeleton <- 
-  data.frame(timestamp = seq(start_date, 
-                             end_date, 
+  data.frame(timestamp = seq(start_date_time_p, 
+                             end_date_time_p, 
                              by = "1 hour"))
+# str(df1.skeleton)
+# summary(df1.skeleton)
 
 df1.skeleton %>% 
   datatable(extensions = 'Buttons',
             options = list(dom = 'Bfrtip', 
                            buttons = c('excel', "csv")))
 
-
-#' 
+#+ denodo 
+#' **************************************************
 #' # ED Start times 
 #' 
 
-df2.start_times <- 
-  ed_data %>% 
-  filter()
+# ED Start times -----
+df2.0.start_times <- 
+  vw_eddata %>% 
+  filter(facility_short_name == "LGH", 
+         start_date_id >= start_date_id_p, 
+         start_date_id <= end_date_id_p) %>% 
+  select(patient_id, 
+         start_date_id, 
+         start_dt_tm) %>% 
+  arrange(start_dt_tm) %>% 
+  collect()
+
+# str(df2.0.start_times)
+
+df2.start_times_grouped <- 
+  df2.0.start_times %>% 
+  mutate(start_hour_floor = floor_date(start_dt_tm, 
+                                       "hour")) %>% 
+  count(start_hour_floor, 
+        name = "num_start")
+
+str(df2.start_times_grouped)
+
+#' **************************************************
+#' # ED End times 
+#' 
+
+# ED End times----------
+df3.0.end_times <- 
+  vw_eddata %>% 
+  filter(facility_short_name == "LGH", 
+         disch_date_id >= start_date_id_p, 
+         disch_date_id <= end_date_id_p) %>% 
+  select(patient_id, 
+         disch_date_id, 
+         disch_dt_tm) %>% 
+  arrange(disch_dt_tm) %>% 
+  collect()
+
+# str(df3.0.end_times)
+
+df3.end_times_grouped <- 
+  df3.0.end_times %>% 
+  mutate(end_hour_floor = floor_date(disch_dt_tm,
+                                     "hour")) %>% 
+  
+  count(end_hour_floor, 
+        name = "num_end")
+
+str(df3.end_times_grouped)
+
+#+ join 
+#' **************************************************
+#' # Join back to the skeleton  
+#' 
+# Join back to the skeleton--------------
+
+df4.net_changes <- 
+  df1.skeleton %>% 
+  left_join(df2.start_times_grouped, 
+            by = c("timestamp" = "start_hour_floor")) %>% 
+  left_join(df3.end_times_grouped, 
+            by = c("timestamp" = "end_hour_floor")) %>% 
+  
+  mutate(net_change = num_start - num_end, 
+         queue_length = cumsum(net_change), 
+         hour_of_day = hour(timestamp)) %>% 
+  
+  select(timestamp, 
+         hour_of_day, 
+         everything())
+
+# str(df4.net_changes)
+# summary(df4.net_changes)
+
+# sum(df4.net_changes$net_change)
+
+# view: 
+df4.net_changes %>% 
+  datatable(extensions = 'Buttons',
+            options = list(dom = 'Bfrtip', 
+                           buttons = c('excel', "csv")))
+
