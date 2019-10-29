ac9e6b442f7af15a80b680bde8fcd87fa84d641f
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-10-28 19:57:41 -0700

    Update 2019-10-25_lgh_ed-mhsu-visits-projection.R
    Parametrize site + plots of daily MHSU ED visits

--- src/2019-10-25_lgh_ed-mhsu-visits-projection.R
+++ src/2019-10-25_lgh_ed-mhsu-visits-projection.R
@@ -7,9 +7,10 @@
 #' output: 
 #'   html_document: 
 #'     keep_md: yes
-#'     code_folding: show
+#'     code_folding: hide
 #'     toc: true
 #'     toc_float: true
+#'     toc_folding: false
 #' ---
 #' 
 
@@ -30,13 +31,14 @@ setup_denodo()
 #' 
 begin_date_id <- "20140101" 
 end_date_id <- "20191001"
+site_param <- "LGH"
 
 #' # Data 
 #' 
 
 df1.visits <- 
   vw_eddata %>% 
-  filter(facility_short_name == "LGH", 
+  filter(facility_short_name == site_param, 
          start_date_id >= begin_date_id, 
          start_date_id <= end_date_id) %>% 
   select(start_date_id,
@@ -100,6 +102,59 @@ df2.mhsu_visits %>%
             options = list(dom = 'Bfrtip', 
                            buttons = c('excel', "csv")))
 
+#' ## Top 5 LHA only
+#' 
+#' 
+
+df2.mhsu_visits %>% 
+  
+  # top 5 levels: 
+  mutate(lha = fct_lump(lha, n = 5)) %>% 
+  count(lha, 
+        year(start_date)) %>% 
+  arrange(lha, `year(start_date)`) %>% 
+  datatable(extensions = 'Buttons',
+            options = list(dom = 'Bfrtip', 
+                           buttons = c('excel', "csv")))
+
+#*************************************************************  
+# graph daily MHSU visits by LHA: 
+p <- 
+  df2.mhsu_visits %>% 
+  
+  # top 5 levels: 
+  mutate(lha = fct_lump(lha, n = 5)) %>% 
+  count(lha, 
+        start_date) %>% 
+  
+  # plot
+  ggplot(aes(x = `start_date`, 
+             y = n, 
+             group = lha, 
+             colour = lha)) + 
+  # geom_point(alpha = .01) + 
+  geom_smooth(se = FALSE) + 
+  
+  scale_color_brewer(type = "qual", 
+                     palette = "Set1") + 
+  
+  coord_cartesian(ylim = c(0,8)) + 
+  
+  labs(title = sprintf("%s: Estimated average daily MHSU ED visits - top 5 LHAs", 
+                       site_param), 
+       y = "Number of visits", 
+       x = "Start Date") +
+
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95")); # p
+  
+ggplotly(p)
+  
+  
+  
+#*************************************************************  
+# graph annual MHSU visits by LHA:   
 p <- 
   df2.mhsu_visits %>% 
   
@@ -108,20 +163,37 @@ p <-
   count(lha, 
         year(start_date)) %>% 
   
+  # plot
   ggplot(aes(x = `year(start_date)`, 
              y = n, 
              group = lha, 
              colour = lha)) + 
-  geom_line(); # p
+  geom_line() + 
+  geom_point() + 
   
+  scale_color_brewer(type = "qual",
+                     palette = "Set1") +
+  
+  # coord_cartesian(ylim = c(0,10)) + 
+  labs(title = sprintf("%s: Annual MHSU ED visits - top 5 LHAs", 
+                       site_param)) +
+  
+  theme_light() +
+  theme(panel.grid.minor = element_line(colour = "grey95"), 
+        panel.grid.major = element_line(colour = "grey95")) # p
+
 ggplotly(p)
-  
-  
-  
-  
-  
-  # ggplot(aes(x = start_date, 
-  #            y = count)) + 
-  # geom_point() + 
-  # geom_smooth() + 
-  # facet_wrap(~patient_geographical_local_health_authority)
+
+
+
+#' # Appendix 
+#' 
+
+# outputs 
+# df2.mhsu_visits %>% 
+#   count(lha, 
+#         year(start_date)) %>% 
+#   write_csv(here::here("results", 
+#                        "dst", 
+#                        "2019-10-25_lgh_mhsu-ed-visits-by-lha_historical.csv"))
+#                
